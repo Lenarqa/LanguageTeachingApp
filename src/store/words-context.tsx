a24ddaf318169a66f7e476d4665563e73f5c6aa3
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-02-20 00:13:27 +0700

    fetch data from api

--- src/store/words-context.tsx
+++ src/store/words-context.tsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { IInitData } from "../models/models";
 
+const SENTENCE_QUERY = `
+{
+    sentenceAll {
+      en,
+      ru
+    }
+}
+`;
+
 type contextObject = {
   data: IInitData;
   setData(newData: IInitData): void;
@@ -22,16 +31,38 @@ const WordsContextProvider: React.FC = (props) => {
     rowsOrder: [],
   });
 
-  useEffect(()=>{
-    console.log("Hello")
-  }, [])
+  useEffect(() => {
+    console.log("Hello from context");
+    fetch("https://academtest.ilink.dev/graphql", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: SENTENCE_QUERY }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const sentenceAll = data.data.sentenceAll;
+        const sentences: string[] = [];
+        let wordsEn: string[] = [];
+
+        for (let i = 0; i < sentenceAll.length; i++) {
+          sentences.push(sentenceAll[i].ru);
+          wordsEn[i] = sentenceAll[i].en.split(" ");
+        }
+
+        
+      });
+  }, []);
 
   const contextValue = {
     data: wordsData,
     setData: setWordsData,
   };
 
-  return <WordsContext.Provider value={contextValue}>{props.children}</WordsContext.Provider>;
+  return (
+    <WordsContext.Provider value={contextValue}>
+      {props.children}
+    </WordsContext.Provider>
+  );
 };
 
 export default WordsContextProvider;
