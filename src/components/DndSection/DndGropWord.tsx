2113bad96b8dd44c4f9c0cd3cfa3afef48373f52
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-02-26 22:15:03 +0700

    add style

--- src/components/DndSection/DndGropWord.tsx
+++ src/components/DndSection/DndGropWord.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import styled from "styled-components";
-import WordItem from "./WordItem";
+import React, {useContext} from "react";
 import { IWord } from "../../models/models";
-
+import WordItem from "./WordItem";
+import { WordsContext } from "../../store/words-context";
 import { GridDropZone } from "react-grid-dnd";
 
 interface DndGroupWordsProps {
@@ -14,20 +13,35 @@ interface DndGroupWordsStyledProps {
   id: string;
 }
 
-const DndGroupWordsStyled = styled.div<DndGroupWordsStyledProps>`
-  width: 482px;
-  min-height: 20rem;
-  margin-bottom: 10px;
-  background-color: ${props => props.id === "phrase" ? 'red' : "#eee"};
-`;
-
 const DndGroupWords: React.FC<DndGroupWordsProps> = (props) => {
+  const wordsCtx = useContext(WordsContext)
+  let style = {};
+  let rowHeight = Math.ceil((wordsCtx.curPhrase.phrase.words.length / 5)) * 45;
+  console.log(rowHeight);
+  
+  if(props.id === "phrase") {
+    style = {
+      width: "100%",
+      height: `${rowHeight}px`,
+      backgroundImage:
+        "linear-gradient(rgba(75, 75, 75, 1),rgba(75, 75, 75, 1) 5%, transparent 5%, transparent)",
+      backgroundSize: "100% 40px",
+    }
+  }else {
+    style = {
+      width: "100%",
+      height: `${rowHeight}px`,
+      marginBottom: "79px",
+    }
+  }
+  
+
   return (
-      <GridDropZone style={{height: "10rem", width:"100%"}} id={props.id} boxesPerRow={5} rowHeight={70} >
-        {props.newWords.map((word) => (
-          <WordItem key={word.id} id={word.id} content={word.content} />
-        ))}
-      </GridDropZone>
+    <GridDropZone style={style} id={props.id} boxesPerRow={5} rowHeight={42}>
+      {props.newWords.map((word) => (
+        <WordItem key={word.id} id={word.id} content={word.content} />
+      ))}
+    </GridDropZone>
   );
 };
 
